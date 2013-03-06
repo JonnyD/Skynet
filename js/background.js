d53ed256a5662d67ac636abe6149c7a548ba139c
JonnyD <contact@jonnydevine.com>
2013-03-06 20:10:30 +0000

    Added HTTPRequest function to handle XMLHttpRequest()

--- js/background.js
+++ js/background.js
@@ -5,3 +5,25 @@ function init() {
 		localStorage.updateInterval = 1;
 	}
 }
+
+function HTTPRequest() {
+	var xmlHttp;
+	// Create xmlHttp Object
+	try {
+		// Firefox, Opera 8.0+, Safari
+		xmlHttp = new XMLHttpRequest();
+	} catch (e) {
+		try {
+			// Internet Explorer
+			xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e) {
+			try {
+				xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (e) {
+				console.log("Your browser does not support AJAX!");
+				return false;
+			}
+		}
+	}
+	return xmlHttp;
+}
